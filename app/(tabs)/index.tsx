36bb7323ba533a6603ea7967b854d35ace50f575
Dipayan Khaidem <dipayansingha8@gmail.com>
2026-04-30 00:54:04 +0530

    feature:Custom bottom tab navigator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
 import "@/global.css"
+
 import { Text, View } from "react-native";
 import {Link} from "expo-router";
+import {styled} from "nativewind";
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 
+
+const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-background">
+        <SafeAreaView className="flex-1 bg-background p-5">
             <Text className="text-xl font-bold text-success">
                 Welcome to Nativewind!
             </Text>
@@ -17,11 +22,11 @@ export default function App() {
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white
             p-4">Go to Sign-Up</Link>
 
-            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+            <Link href="/app/subscriptions/spotify">Spotify Subscription</Link>
             <Link href={{
                 pathname:"/subscriptions/[id]",
                 params:{id:"claude"},
             }}>Claude Max Subscription</Link>
-        </View>
+        </SafeAreaView>
     );
 }
